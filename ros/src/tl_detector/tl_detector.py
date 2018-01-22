dbc1dc4edb27a449ada49d1c299fd0365b0b68ec
Unknown <marc.norvig@gmail.com>
2018-01-21 16:04:22 -0800

    Implement dummy tl_detector
    Tl detector now finds closest traffic light ahead of the car and publishes a traffic_waupoint message with the location of the light stop line and the current state of the light. Light state is taken from the ground truth message stream. This will have to be replaced with a call to tl_classifier when it's ready.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -10,6 +10,7 @@ from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
+import math
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -92,7 +93,6 @@ class TLDetector(object):
 
     def get_closest_waypoint(self, pose):
         """Identifies the closest path waypoint to the given position
-            https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
         Args:
             pose (Pose): position to match a waypoint to
 
@@ -100,8 +100,29 @@ class TLDetector(object):
             int: index of the closest waypoint in self.waypoints
 
         """
-        #TODO implement
-        return 0
+        #TODO implement DONE
+        if self.waypoints is None:
+            return
+        
+        # Extract given position info
+        given_x = pose.position.x
+        given_y = pose.position.y
+        
+        # Initialize search
+        closest_wp_dist = float('inf')
+        closest_wp_idx = -1
+        
+        # Exhaustive search
+        for idx, wp in enumerate (self.waypoints.waypoints):
+            x_squared = (wp.pose.pose.position.x - given_x)**2
+            y_squared = (wp.pose.pose.position.y - given_y)**2
+            distance = math.sqrt(x_squared + y_squared)
+            
+            if (distance < closest_wp_dist):
+                closest_wp_dist = distance
+                closest_wp_idx = idx
+
+        return closest_wp_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -131,20 +152,79 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        light = None
-
-        # List of positions that correspond to the line to stop in front of for a given intersection
-        stop_line_positions = self.config['stop_line_positions']
+        # Hyper-Parameters
+        max_visible_distance = 60   # Anything further is too pay attention to
+        
+        # Initialize
+        waypoint_idx = None
+        light_color  = TrafficLight.UNKNOWN
+        
+        # Exit early if no pose info
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+            ego_position = self.pose.pose.position
+            ego_orientation = self.pose.pose.orientation
+        else:
+            return waypoint_idx, light_color
+        
+        # Find closest traffic_light stop_line that's ahead of the ego vehicle
+        stop_line_positions = self.config['stop_line_positions']
+        
+        closest_dist = max_visible_distance
+        closest_idx = -1     # Negative value indicates no light found
+        
+        # Iterate through all stop line positions
+        for i, line_loc in enumerate(stop_line_positions):
+            # Get stop line location
+            line_x = line_loc[0]
+            line_y = line_loc[1]
+            
+            # Calculate distance to line
+            x_squared = (ego_position.x - line_x)**2
+            y_squared = (ego_position.y - line_y)**2
+            line_distance = math.sqrt(x_squared + y_squared)
+            
+            # Check if current line is closer than the previous closest line
+            if line_distance < closest_dist:
 
-        #TODO find the closest visible traffic light (if one exists)
-
-        if light:
-            state = self.get_light_state(light)
-            return light_wp, state
-        self.waypoints = None
-        return -1, TrafficLight.UNKNOWN
+                # Check if line is ahead of vehicle.
+                # The inner product of the car's heading (yaw) and a vector 
+                # between the car and the stop line will be positive if the 
+                # line is ahead of the vehicle. Yaw angle transform taken from
+                # https://answers.ros.org/question/69754/quaternion-transformations-in-python/
+                quaternion = (
+                    ego_orientation.x,
+                    ego_orientation.y,
+                    ego_orientation.z,
+                    ego_orientation.w)
+                euler = tf.transformations.euler_from_quaternion(quaternion)
+                yaw = euler[2]
+                
+                yaw_vec_x = math.cos(yaw)
+                yaw_vec_y = math.sin(yaw)
+                line_vec_x = line_x - ego_position.x
+                line_vec_y = line_y - ego_position.y
+                inner_prod = yaw_vec_x*line_vec_x + yaw_vec_y*line_vec_y
+                
+                if inner_prod > 0:
+                    # Update closest line info
+                    closest_x = line_x
+                    closest_y = line_y
+                    closest_idx = i
+                    closest_dist = line_distance
+                    
+        # Process light (if we found one)
+        if closest_idx >= 0:
+            line_pose = Pose()
+            line_pose.position.x = closest_x
+            line_pose.position.y = closest_y
+            waypoint_idx = self.get_closest_waypoint(line_pose)
+            closest_light = self.lights[closest_idx]
+            # FIXME Temporary ground truth passthrough while get_classification method is in development.
+            light_color = closest_light.state
+            # light_color = get_light_state(closest_light)
+            print ("Light Waypoint: ", waypoint_idx, " Color: ", light_color)
+            
+        return waypoint_idx, light_color
 
 if __name__ == '__main__':
     try:
